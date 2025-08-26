6f8bb57d716c7c4a5b591c16e06752f9708917c6
Yusi <yusi4781@naver.com>
2025-08-26 14:00:16 +0900

    feat: 비밀번호 재설정 route 설정 - #9

---
+++ src/pages/ResetPassword.tsx
@@ -0,0 +1,9 @@
+const ResetPassword = () => {
+  return (
+    <div className="w-full h-[100vh] flex flex-col items-center justify-center">
+      ResetPassword
+    </div>
+  );
+};
+
+export default ResetPassword;
