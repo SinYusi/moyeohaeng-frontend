23b1d8da520720465ffe64aeca812589a636f6b0
Yusi <yusi4781@naver.com>
2025-09-05 16:35:40 +0900

    style: 공통 컴포넌트 Divider 마진 제거 및 tailwind css 사용 가능하게 수정 - #6

--- src/components/common/Divider.tsx
+++ src/components/common/Divider.tsx
@@ -1,19 +1,21 @@
-const Divider = ({ label, color }: { label?: string; color?: string }) => {
+const Divider = ({
+  label,
+  color,
+  className,
+}: {
+  label?: string;
+  color?: string;
+  className?: string;
+}) => {
   return (
-    <div className="flex items-center my-4 w-full">
-      <div
-        className={`flex-grow border-t`}
-        style={{ borderColor: color }}
-      />
+    <div className={`flex items-center w-full ${className}`}>
+      <div className={`flex-grow border-t`} style={{ borderColor: color }} />
       {label && (
         <span className={`text-sm mx-4`} style={{ color: color }}>
           {label}
         </span>
       )}
-      <div
-        className={`flex-grow border-t`}
-        style={{ borderColor: color }}
-      />
+      <div className={`flex-grow border-t`} style={{ borderColor: color }} />
     </div>
   );
 };
