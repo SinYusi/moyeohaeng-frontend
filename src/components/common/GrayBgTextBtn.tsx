39a1ca4d76859a8111de038a1683370fb0b7fd64
Yusi <yusi4781@naver.com>
2025-09-08 02:05:06 +0900

    feat: 회색 바탕 텍스트 버튼 onClick 구현 - #6

--- src/components/common/GrayBgTextBtn.tsx
+++ src/components/common/GrayBgTextBtn.tsx
@@ -1,6 +1,15 @@
-const GrayBgTextBtn = ({ children }: { children: React.ReactNode }) => {
+const GrayBgTextBtn = ({
+  children,
+  onClick,
+}: {
+  children: React.ReactNode;
+  onClick?: () => void;
+}) => {
   return (
-    <button className="cursor-pointer p-1 hover:bg-[#f9fafb] active:bg-[#e7edf6] flex items-center gap-2 rounded-[4px]">
+    <button
+      className="cursor-pointer p-1 hover:bg-[#f9fafb] active:bg-[#e7edf6] flex items-center gap-2 rounded-[4px]"
+      onClick={onClick}
+    >
       {children}
     </button>
   );
