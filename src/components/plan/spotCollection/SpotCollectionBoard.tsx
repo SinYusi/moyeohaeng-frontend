5fa9d5f6ede7c2dad16f82b4d4b8717168b20e89
Yusi <yusi4781@naver.com>
2025-09-10 09:51:19 +0900

    feat: 장소블록이 없을 때 빈 화면 처리

--- src/components/plan/spotCollection/SpotCollectionBoard.tsx
+++ src/components/plan/spotCollection/SpotCollectionBoard.tsx
@@ -1,7 +1,23 @@
 import GroupSection from "./GroupSection";
 import AllBlockSection from "./AllBlockSection";
+import { useSpotCollectionStore } from "../../../stores/useSpotCollectionStore";
+import emptyCollection from "../../../assets/images/empty.png";
 
 const SpotCollectionBoard = () => {
+  const { collections } = useSpotCollectionStore();
+  if (collections.length === 0)
+    return (
+      <div className="w-full h-full px-9 py-6 flex flex-col items-center justify-center gap-8">
+        <img
+          src={emptyCollection}
+          alt="emptyCollection"
+          className="w-[70%]"
+        />
+        <p className="text-[24px] text-[#131416] font-[700]">
+          마음에 드는 장소를 모아 함께 이야기해 보세요.
+        </p>
+      </div>
+    );
   return (
     <div className="w-full px-9 py-6 flex flex-col items-center gap-8">
       <GroupSection />
